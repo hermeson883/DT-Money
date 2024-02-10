c622e71649e09ca14529b993600d9f7c8599fe06
hermeson883 <hermesondovalebeserra722@gmail.com>
2024-02-10 15:35:10 -0300

    Adding radix UI

--- src/components/Header/Index.tsx
+++ src/components/Header/Index.tsx
@@ -1,12 +1,20 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './style'
 import logo from '../../assets/logo.svg'
-
+import * as Dialog from '@radix-ui/react-dialog'
+import { NewTransactionModal } from '../NewTransactionModal/Index'
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logo} alt="Logo" />
-        <NewTransactionButton>Nova Transação</NewTransactionButton>
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova Transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
--- src/components/NewTransactionModal/Index.tsx
+++ src/components/NewTransactionModal/Index.tsx
@@ -1,13 +1,18 @@
 import * as Dialog from '@radix-ui/react-dialog'
-import { Overlay, Content } from './style'
+import { Overlay, Content, CloseButton } from './style'
+import { X } from 'phosphor-react'
 
-export function Index() {
+export function NewTransactionModal() {
   return (
     <Dialog.Portal>
       <Overlay />
 
       <Content>
         <Dialog.Title>Nova transação</Dialog.Title>
+
+        <CloseButton>
+          <X size={24} />
+        </CloseButton>
 
         <form action="">
           <input type="text" placeholder="Descrição" required />
@@ -16,8 +21,6 @@ export function Index() {
 
           <button type="submit">Cadastrar</button>
         </form>
-
-        <Dialog.Close />
       </Content>
     </Dialog.Portal>
   )
